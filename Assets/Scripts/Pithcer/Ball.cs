c4dc601d2902bb4688cc5d69a267900493204084
junjun1002 <ueno.junya@outlook.jp>
2021-10-23 18:24:46 +0900

    球種がだいぶ増えた カーブはえげつないので変えます

--- Assets/Scripts/Pithcer/Ball.cs
+++ Assets/Scripts/Pithcer/Ball.cs
@@ -16,15 +16,23 @@ public class Ball : MonoBehaviour
     [SerializeField] public BallType m_ballType;
     /// <summary>ボールのスピード</summary>
     [SerializeField] float m_speed;
-
+    /// <summary>変化させるときの力</summary>
     [SerializeField] float m_changePower = 100;
+    /// <summary>変化させるタイミング</summary>
+    [SerializeField] float m_changeTime = 0.6f;
 
-    /// <summary>ストレートの力</summary>
+    /// <summary>ストレートの力の方向</summary>
     [SerializeField] Vector3 m_straightDirection = new Vector3(0f, 0.2f, 1.0f);
-
-    [SerializeField] Vector3 m_curveDirection;
-
-    [SerializeField] Vector3 m_sliderDirection = new Vector3(1.0f, -1.0f, 0f);
+    /// <summary>カーブの最初の力の方向</summary>
+    [SerializeField] Vector3 m_curveDirection = new Vector3(0.05f, 0.25f, 0f);
+    /// <summary>カーブの変化させるときに加える力の方向</summary>
+    [SerializeField] Vector3 m_changeCurveDirection = new Vector3(-0.1f, -1.0f, 0f);
+    /// <summary>スライダー変化させるときに加える力の方向</summary>
+    [SerializeField] Vector3 m_sliderDirection = new Vector3(-1.2f, -1.0f, 0f);
+    /// <summary>シュート変化させるときに加える力の方向</summary>
+    [SerializeField] Vector3 m_shootDirection = new Vector3(1.2f, 0f, 0f);
+    /// <summary>フォーク変化させるときに加える力の方向</summary>
+    [SerializeField] Vector3 m_forkDirection = new Vector3(0f, -2.0f, 0f);
 
     Rigidbody m_rb;
 
@@ -42,11 +50,24 @@ public class Ball : MonoBehaviour
                 m_rb.AddForceAtPosition(m_straightDirection * m_speed, m_catcherPos.transform.position);
                 break;
             case BallType.Curve:
+                m_rb.AddForceAtPosition(m_curveDirection * m_speed, m_catcherPos.transform.position);
+                yield return new WaitForSeconds(m_changeTime);
+                m_rb.AddForceAtPosition(m_changeCurveDirection * m_changePower, m_catcherPos.transform.position);
                 break;
             case BallType.Slider:
                 m_rb.AddForceAtPosition(m_straightDirection * m_speed, m_catcherPos.transform.position);
-                yield return new WaitForSeconds(0.6f);
+                yield return new WaitForSeconds(m_changeTime);
                 m_rb.AddForceAtPosition(m_sliderDirection * m_changePower, m_catcherPos.transform.position);
+                break;
+            case BallType.Shoot:
+                m_rb.AddForceAtPosition(m_straightDirection * m_speed, m_catcherPos.transform.position);
+                yield return new WaitForSeconds(m_changeTime);
+                m_rb.AddForceAtPosition(m_shootDirection * m_changePower, m_catcherPos.transform.position);
+                break;
+            case BallType.Fork:
+                m_rb.AddForceAtPosition(m_straightDirection * m_speed, m_catcherPos.transform.position);
+                yield return new WaitForSeconds(m_changeTime);
+                m_rb.AddForceAtPosition(m_forkDirection * m_changePower, m_catcherPos.transform.position);
                 break;
             default:
                 break;
@@ -86,5 +107,7 @@ public enum BallType
 {
     Straight = 0,
     Curve = 1,
-    Slider = 2
+    Slider = 2,
+    Shoot = 3,
+    Fork = 4
 }
