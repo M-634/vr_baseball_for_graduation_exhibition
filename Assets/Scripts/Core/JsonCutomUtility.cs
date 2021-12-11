9279369c56264dfdd3b73901f45dffe4c4a1eaf3
M-634 <miyanagateru634@gmail.com>
2021-12-11 19:33:10 +0900

    ランキング機能の基幹部分の実装をしました

---
+++ Assets/Scripts/Core/JsonCutomUtility.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization;
+
+
+public static class JsonCutomUtility
+{
+    public interface ISerializeToJson { }
+    public static void SaveDataToJson<T>(string path, T data) where T : ISerializeToJson
+    {
+        var json = JsonUtility.ToJson(data, true);
+
+        try
+        {
+            using var writer = new StreamWriter(path, false);
+            writer.Write(json);
+            Debug.Log($"Saving {data}...");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+
+    public static bool LoadDataFromJson<T>(string path, ref T data) where T : ISerializeToJson
+    {
+        try
+        {
+            using var reader = new StreamReader(path);
+            var json = reader.ReadToEnd();
+            data = JsonUtility.FromJson<T>(json);
+
+            Debug.Log($"Loading {data}...");
+            return true;
+
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+            return false;
+        }
+    }
+}
