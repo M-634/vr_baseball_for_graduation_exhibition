b124fbaf6f3a9e2c74fc63470e5260c522c73774
Ykatsu999 <dai1nokira@gmail.com>
2021-12-17 18:40:11 +0900

    ランナーの速度がPCによって異なる不具合を修正

--- Assets/Scripts/Core/RunnnerController.cs
+++ Assets/Scripts/Core/RunnnerController.cs
@@ -18,6 +18,7 @@ public class RunnnerController : MonoBehaviour
     [SerializeField] GameObject m_base3;
     Dictionary<BaseName, Vector3> m_basePositions = new Dictionary<BaseName, Vector3>();
     [SerializeField] float m_moveSpeed = 1.0f;
+    [SerializeField] Animator m_runnerAnimController;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +28,6 @@ public class RunnnerController : MonoBehaviour
         {
             BaseBallLogic.Instance.OnProcessRunner += Hit;
         }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
 
     }
 
@@ -49,11 +43,18 @@ public class RunnnerController : MonoBehaviour
         m_basePositions.Add(BaseName.Third, m_base3.transform.position);//3塁の座標
     }
 
+    /// <summary>
+    /// ランナーを動かす処理を管理している
+    /// </summary>
+    /// <param name="hitNum">ヒット=1、ツーベースヒット=2、スリーベースヒット=3、ホームラン=4</param>
+    /// <returns></returns>
     async UniTask RunnerMove(int hitNum)
     {
+        if (hitNum <= 0 && hitNum > 4) return;
+
         var uniTasks = new List<UniTask>();
-        //ランナーインスタンスを追加する
-        m_runners.Add(new Runner(BaseName.Home, Instantiate(m_runnerObj)));
+        //新たに出塁する(元バッター)ランナーのインスタンスを追加する
+        m_runners.Add(new Runner(BaseName.Home, Instantiate(m_runnerObj), m_runnerAnimController));
         //すべてのランナーを走らせる
         for (int i = 0; i < m_runners.Count; i++)
         {
@@ -63,9 +64,9 @@ public class RunnnerController : MonoBehaviour
                 var nowBase = m_runners[i].GetBasePosi;
                 if (nowBase == BaseName.Third)
                 {
-                    m_runners[i].GetOnBase();//ホームに進む
+                    m_runners[i].ToNextBase();//ホームに進む
                     m_runners[i].RegisterRelayPoint(m_basePositions[BaseName.Home]);
-                    uniTasks.Add(RunningAnimTask(m_runners[i], BaseName.Home, m_moveSpeed));
+                    uniTasks.Add(RunningAnim(m_runners[i], BaseName.Home, m_moveSpeed));
                     m_runners.RemoveAt(i);
                     i--;
                     break;
@@ -73,11 +74,11 @@ public class RunnnerController : MonoBehaviour
                 else
                 {
                     nowBase++;
-                    m_runners[i].GetOnBase();//次の塁に進む
+                    m_runners[i].ToNextBase();//次の塁に進む
                     m_runners[i].RegisterRelayPoint(m_basePositions[nowBase]);
                     if (j == hitNum - 1)
                     {
-                        uniTasks.Add(RunningAnimTask(m_runners[i], nowBase, m_moveSpeed));
+                        uniTasks.Add(RunningAnim(m_runners[i], nowBase, m_moveSpeed));
                     }
                 }
             }
@@ -113,7 +114,7 @@ public class RunnnerController : MonoBehaviour
                 break;
         }
         await RunnerMove(advanceBases);
-        Debug.Log("Unitask完了! ランナーの数は : " + m_runners.Count);
+        Debug.Log("ランナーの処理完了! ランナーの数は : " + m_runners.Count);
     }
 
     /// <summary>
@@ -126,7 +127,7 @@ public class RunnnerController : MonoBehaviour
         {
             Destroy(m_runners[i].GetRunnerObj);
         }
-        Debug.Log("スリーアウト！");
+        Debug.Log("ランナーをリセットした");
     }
 
     /// <summary>
@@ -137,7 +138,7 @@ public class RunnnerController : MonoBehaviour
     /// <param name="speed"></param>
     /// <param name="cancellation_token"></param>
     /// <returns></returns>
-    async UniTask RunningAnimTask(Runner runner, BaseName nextBase, float speed, CancellationToken cancellation_token = default)
+    async UniTask RunningAnim(Runner runner, BaseName nextBase, float speed, CancellationToken cancellation_token = default)
     {
         var moveTime = 0.0f;
         Vector3 finishPosi;//移動先の座標
@@ -183,7 +184,7 @@ public class RunnnerController : MonoBehaviour
         rotatePoint = m_basePositions[BaseName.Home];
         rotatePoint.y = nowPosi.y;
         runner.GetRunnerObj.transform.LookAt(rotatePoint);
-        return;
+        runner.ReachOnBase();
     }
 
 #if UNITY_EDITOR
@@ -220,15 +221,25 @@ public enum BaseName
     Home, First, Second, Third
 }
 
+public enum RunnerState
+{
+    Idle,Running
+}
+
 
 /// <summary>
 /// 走る人
 /// </summary>
 public class Runner
 {
+    /// <summary> どこのベースにいるか </summary>
     BaseName m_nowBaseName = BaseName.Home;
+    RunnerState m_runnerState = RunnerState.Idle;
     GameObject m_body;
-    List<Vector3> m_relayPoints = new List<Vector3>();//中継地点の登録リスト
+    /// <summary> 中継地点の登録リスト </summary>
+    List<Vector3> m_relayPoints = new List<Vector3>();
+    /// <summary> ランナーのアニメーターを格納する。走っているモーションや待機中のモーションをここに格納し、引用する </summary>
+    Animator m_runnerAnim;
 
     public BaseName GetBasePosi
     {
@@ -254,23 +265,46 @@ public class Runner
         }
     }
 
+    public Animator GetAnimator
+    {
+        get
+        {
+            return m_runnerAnim;
+        }
+    }
+
+    public RunnerState GetRunnerState
+    {
+        get
+        {
+            return m_runnerState;
+        }
+    }
+
     /// <summary>
     /// コンストラクター
     /// </summary>
     /// <param name="basePosi"></param>
-    public Runner(BaseName basePosi, GameObject body)
+    public Runner(BaseName basePosi, GameObject body, Animator animator)
     {
         m_nowBaseName = basePosi;
         m_body = body;
+        m_runnerAnim = animator;
     }
 
     /// <summary>
     /// ランナーが出塁する
     /// </summary>
     /// <param name="totalBases">塁打数</param>
-    public void GetOnBase()
+    public void ToNextBase()
     {
         m_nowBaseName++;
+        m_runnerState = RunnerState.Running;
+    }
+
+    public void ReachOnBase()
+    {
+        m_runnerState = RunnerState.Idle;
     }
 
     /// <summary>
@@ -286,7 +320,4 @@ public class Runner
     {
         m_relayPoints.Clear();
     }
-
-    
 }
-
