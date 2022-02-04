7c34cfbceda1af5d28e1da82df40ab822edd9f7c
M-634 <miyanagateru634@gmail.com>
2022-02-04 15:32:06 +0900

    VR上でランキング機能を実装する準備

---
+++ Assets/Scripts/Core/RankingManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// PlayFabとの通信のやり取り、UIへの出力を制御するクラス
+/// </summary>
+public class RankingManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
